dacca66847e9b099ef64a7f1b3da5f3ce8ce6fe9
Smiling Dog <a.kudr74@gmail.com>
2022-12-31 18:47:59 +0200

    added live search from searchParams

--- src/pages/Blogpage.jsx
+++ src/pages/Blogpage.jsx
@@ -1,10 +1,15 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 const POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
 
 export const Blogpage = () => {
   const [posts, setPosts] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const postQuery = searchParams.get("post") || "";
+
+  const handleChange = (e) => setSearchParams({ post: e.target.value });
 
   const getPosts = async () => {
     const res = await fetch(POSTS_URL);
@@ -17,16 +22,25 @@ export const Blogpage = () => {
   }, []);
   //todo all actions are passed INSIDE a callback in useEffect
 
-  const postsList = posts?.map((p) => (
-    <Link key={p.id} to={`/posts/${p.id}`}>
-      <li>{p.title}</li>
-    </Link>
-  ));
+  const postsList = posts
+    ?.filter((p) => p.title.includes(postQuery))
+    .map((p) => (
+      <Link key={p.id} to={`/posts/${p.id}`}>
+        <li>{p.title}</li>
+      </Link>
+    ));
 
   return (
     <div>
       <h1>Blog</h1>
-      <Link to={'/posts/new'} className='add-post'>Add new post</Link>
+      <label>
+        Type to search: <input type="text" value={postQuery} onChange={handleChange} />
+      </label>
+      <div className="input-container">
+        <Link to={"/posts/new"} className="add-post">
+          Add new post
+        </Link>
+      </div>
       <ul>{postsList}</ul>
     </div>
   );

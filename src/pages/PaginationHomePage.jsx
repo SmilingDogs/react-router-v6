8e13d2b27aa7b0182b4b50f81577ae91fdd15033
Oleksiy Kudryashov <oleksiy.kudryashov@valtech.com>
2023-01-04 12:37:09 +0200

    added debounced search

--- src/pages/PaginationHomePage.jsx
+++ src/pages/PaginationHomePage.jsx
@@ -2,20 +2,27 @@ const BASE_URL = "http://hn.algolia.com/api/v1/search?";
 
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { Link as NavLink, useNavigate, useLocation } from 'react-router-dom';
-import { Pagination, TextField, Stack, Link, PaginationItem } from "@mui/material";
+import { Link as NavLink, useNavigate, useLocation } from "react-router-dom";
+import {
+  Pagination,
+  TextField,
+  Stack,
+  Link,
+  PaginationItem,
+} from "@mui/material";
 
 import React from "react";
+import { useDebounce } from "../hooks/useDebounce";
 
 export const PaginationHomePage = () => {
   const [posts, setPosts] = useState([]);
   const [query, setQuery] = useState("react");
   const location = useLocation();
   const navigate = useNavigate();
-  const [page, setPage] = useState(parseInt(location.search?.split('=')[1]) || 1);
+  const [page, setPage] = useState(parseInt(location.search?.split("=")[1]) || 1);
   const [pageQty, setPageQty] = useState(0);
 
-  useEffect(() => {
+  const debouncedCallback = useDebounce(() => {
     axios
       .get(BASE_URL + `query=${query}&page=${page - 1}`, {
         headers: {
@@ -29,45 +36,45 @@ export const PaginationHomePage = () => {
 
         if (data.nbPages < page) {
           setPage(1);
-          navigate('/', {replace: true});
+          navigate("/", { replace: true });
         }
       });
-  }, [query, page]);
+  }, 1000);
+
+  useEffect(debouncedCallback, [query, page]); //todo here we do not call the debouncedCallback, just put the reference
 
   return (
     <>
-     
-        <TextField
-          fullWidth
-          label="query"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <Stack spacing={2}>
-          {!!pageQty && (
-            <Pagination
-              count={pageQty}
-              page={page}
-              onChange={(_, num) => setPage(num)}
-              showFirstButton
-              showLastButton
-              sx={{ marfinY: 3, marginX: "auto" }}
-              renderItem={(item) => (
-                <PaginationItem 
-                    component={NavLink}
-                    to={`/?page=${item.page}`}
-                    {...item}
-                />
-              )}
-            />
-          )}
-          {posts?.map((post) => (
-            <Link key={post.objectID} className="post">
-              {post.title || post.story_title}
-            </Link>
-          ))}
-        </Stack>
-     
+      <TextField
+        fullWidth
+        label="query"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <Stack spacing={2}>
+        {!!pageQty && (
+          <Pagination
+            count={pageQty}
+            page={page}
+            onChange={(_, num) => setPage(num)}
+            showFirstButton
+            showLastButton
+            sx={{ marfinY: 3, marginX: "auto" }}
+            renderItem={(item) => (
+              <PaginationItem
+                component={NavLink}
+                to={`/?page=${item.page}`}
+                {...item}
+              />
+            )}
+          />
+        )}
+        {posts?.map((post) => (
+          <Link key={post.objectID} className="post">
+            {post.title || post.story_title}
+          </Link>
+        ))}
+      </Stack>
     </>
   );
 };
